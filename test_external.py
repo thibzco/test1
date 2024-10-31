17f2a27f5bb6006d394de20732f5100d6ebe64b2
Thibaud <dthibzco@gmail.com>
2024-10-31 10:39:36 -0600

    Test2

--- test_external.py
+++ test_external.py
@@ -2,4 +2,7 @@ import streamlit as st
 
 st.write("Hello from Streamlit!")
 
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
 print("external python file")
