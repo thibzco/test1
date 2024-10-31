40cbcacedabbf5f22e4a99c6f97b8893e7d3a759
Thibaud <dthibzco@gmail.com>
2024-10-31 14:29:28 -0600

    TestOpenAIchat

--- test_external.py
+++ test_external.py
@@ -1,32 +1,64 @@
+# import streamlit as st
+
+
+# st.title("BP test")
+# #st.write("Hello from Streamlit!")
+
+
+# # Initialize chat history
+# if "messages" not in st.session_state:
+#     st.session_state.messages = []
+
+# # Display chat messages from history on app rerun
+# for message in st.session_state.messages:
+#     with st.chat_message(message["role"]):
+#         st.markdown(message["content"])
+# # message = st.chat_message("assistant")
+# # message.write("Hello human")
+
+# if prompt := st.chat_input("What is up?"):
+#     # Display user message in chat message container
+#     st.chat_message("user").markdown(prompt)
+#     # Add user message to chat history
+#     st.session_state.messages.append({"role": "user", "content": prompt})
+
+#     response = f"Echo: {prompt}"
+#     # Display assistant response in chat message container
+#     with st.chat_message("BP"):
+#         st.markdown(response)
+#     # Add assistant response to chat history
+#     st.session_state.messages.append({"role": "BP", "content": response})
+
+from openai import OpenAI
 import streamlit as st
 
+st.title("ChatGPT-like clone")
 
-st.title("BP test")
-#st.write("Hello from Streamlit!")
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-# Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
-# message = st.chat_message("assistant")
-# message.write("Hello human")
 
 if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
-    st.chat_message("user").markdown(prompt)
-    # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-    response = f"Echo: {prompt}"
-    # Display assistant response in chat message container
-    with st.chat_message("BP"):
-        st.markdown(response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "BP", "content": response})
-
-    
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
