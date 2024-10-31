68888eaa3f9736733ba9fb1c6fab399367a1482f
Thibaud <dthibzco@gmail.com>
2024-10-31 13:27:28 -0600

    TestSession

--- test_external.py
+++ test_external.py
@@ -4,9 +4,15 @@ import streamlit as st
 st.title("BP test")
 #st.write("Hello from Streamlit!")
 
+
+# Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 # message = st.chat_message("assistant")
 # message.write("Hello human")
 
