038ed25f3e533347b75c5b21998fbb4e855bbf48
Thibaud <dthibzco@gmail.com>
2024-10-31 10:56:49 -0600

    TestMessage

--- test_external.py
+++ test_external.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 st.write("Hello from Streamlit!")
 
+message = st.chat_message("assistant")
+message.write("Hello human")
+
 prompt = st.chat_input("Say something")
 if prompt:
     st.write(f"User has sent the following prompt: {prompt}")
